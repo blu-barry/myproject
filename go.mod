597c93ff6197faa99915f6d2f04927c585cee19f
Michael Barry <michael23barry@gmail.com>
2023-03-22 12:51:59 -0400

    Properly set up go project with the usae of a private module.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/blu-barry/myproject
+
+go 1.20
+
+require github.com/blu-barry/mysecret v0.0.0-20230322164112-9ee21230fafb
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/blu-barry/mysecret"
+)
+
+func main() {
+	fmt.Println("My new project!")
+	mysecret.SecretProcess()
+}
